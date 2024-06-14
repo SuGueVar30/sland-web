7094e3d3d7c2d0b0fb41cf4f25ddc787d4e097a9
SuGueVar30 <susoguevar@gmail.com>
2024-06-13 19:23:05 -0500

    Actualización_06

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,18 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'ca', 'en'],
+    fallbackLocale: 'es',
+    localeDetection: true,
+    routing: {
+      prefixDefaultLocale: false,
+    },
+    fallback: {
+      'ca': 'es',
+      'en': 'es'
+    }
+  }
 });
---
+++ src/i18n/index.ts
@@ -0,0 +1,18 @@
+import catalan from './ca.json'
+import spanish from './es.json'
+import english from './en.json'
+
+const LANGUAGES = {
+  CATALAN: 'ca',
+  SPANISH: 'es',
+  ENGLISH: 'en'
+}
+
+export const getI18N = (
+  { currentLocale = 'es' } : 
+  { currentLocale: string | undefined }
+) => {
+  if (currentLocale === LANGUAGES.CATALAN) return catalan
+  if (currentLocale === LANGUAGES.ENGLISH) return english
+  return spanish
+}
